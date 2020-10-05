3084b75b256c5ba0db27bb8d4149868bef2b7826
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-10-06 00:29:32 +0200

    enable jwt authentication

--- exposition/src/main/java/org/dbs/shop/security/SecurityConfig.java
+++ exposition/src/main/java/org/dbs/shop/security/SecurityConfig.java
@@ -1,39 +1,40 @@
 package org.dbs.shop.security;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsService customUserDetailsService;
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	@Override
-	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+	@Autowired
+	private UserDetailsService jwtUserDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+		// configure AuthenticationManager so that it knows from where to load
+		// user for matching credentials
+		// Use BCryptPasswordEncoder
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
@@ -41,37 +42,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
 	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http.csrf().disable()//
-		.formLogin()//
-		.loginProcessingUrl("/login").successHandler(new AuthentificationLoginSuccessHandler()) //
-		.failureHandler(new SimpleUrlAuthenticationFailureHandler())//
-		.and() //
-		.logout() //
-		.logoutUrl("/logout").logoutSuccessHandler(new AuthentificationLogoutSuccessHandler())
-		.invalidateHttpSession(true) //
-		.and() //
-		.authorizeRequests().antMatchers("/login").permitAll() //
-		.antMatchers("/logout").permitAll()//
-		.antMatchers("/api/**").authenticated().anyRequest().permitAll() //
-		.and().exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint() {
-		});
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
-	private class AuthentificationLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-		@Override
-		public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response,
-				final Authentication authentication) throws IOException, ServletException {
-			response.setStatus(HttpServletResponse.SC_OK);
-		}
+	@Override
+
+	protected void configure(final HttpSecurity httpSecurity) throws Exception {
+		// We don't need CSRF for this example
+		httpSecurity.csrf().disable()
+		// dont authenticate this particular request
+		.authorizeRequests().antMatchers("/authenticate").permitAll().
+		// all other requests need to be authenticated
+		anyRequest().authenticated().and().
+		// make sure we use stateless session; session won't be used to
+		// store user's state.
+		exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		// Add a filter to validate the tokens with every request
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
-	private class AuthentificationLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
-		@Override
-		public void onLogoutSuccess(final HttpServletRequest request, final HttpServletResponse response,
-				final Authentication authentication) throws IOException, ServletException {
-			response.setStatus(HttpServletResponse.SC_OK);
-		}
-	}
 }
